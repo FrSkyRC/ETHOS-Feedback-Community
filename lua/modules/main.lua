6ab55098fb29edf6024afeff1cc815bb7bec12f8
Bertrand Songis @ FrSky <50834778+bsongis-frsky@users.noreply.github.com>
2022-08-26 23:36:06 +0200

    Add files via upload

--- lua/modules/main.lua
+++ lua/modules/main.lua
@@ -10,14 +10,14 @@ local function init()
 
     -- Multimodule protocols
     system.registerMultimoduleProtocol("Assan", 24)
-    system.registerMultimoduleProtocol("Devo", 7, {"8CH", "10CH", "12CH", "6CH", "7CH"})
-    system.registerMultimoduleProtocol("DSM", 6, {"DSM2-1F", "DSM2-2F", "DSMX-1F", "DSMX-2F", "AUTO", "DSMR-1F"})
-    system.registerMultimoduleProtocol("FlySky", 1, {"Default", "V9X9", "V6X6", "V912", "CX20"})
-    system.registerMultimoduleProtocol("Frsky D8", 3)
-    system.registerMultimoduleProtocol("Frsky V", 25)
-    system.registerMultimoduleProtocol("FlySky AFHDS 2A", 28, {"PWM+IBUS", "PPM+IBUS", "PWM+SBUS", "PPM+SBUS", "PWM+IBUS16", "PPM+IBUS16"}, FAILSAFE + TELEMETRY, {SERVO_REFRESH_RATE_OPTION})
-    system.registerMultimoduleProtocol("Hubsan", 2, {"H107", "H301", "H501"})
-    system.registerMultimoduleProtocol("OMP", 77, {}, TELEMETRY, {RF_TUNE_OPTION})
+    system.registerMultimoduleProtocol("Devo", 7, {variants={"8CH", "10CH", "12CH", "6CH", "7CH"}})
+    system.registerMultimoduleProtocol("DSM", 6, {variants={"DSM2-1F", "DSM2-2F", "DSMX-1F", "DSMX-2F", "AUTO", "DSMR-1F"}, minChannels=3, maxChannels=12})
+    system.registerMultimoduleProtocol("FlySky", 1, {variants={"Default", "V9X9", "V6X6", "V912", "CX20"}})
+    system.registerMultimoduleProtocol("FrSky D8", 3, {minChannels=8, maxChannels=8})
+    system.registerMultimoduleProtocol("FrSky V", 25)
+    system.registerMultimoduleProtocol("FlySky AFHDS 2A", 28, {variants={"PWM+IBUS", "PPM+IBUS", "PWM+SBUS", "PPM+SBUS", "PWM+IBUS16", "PPM+IBUS16"}, features=FAILSAFE+TELEMETRY, options={SERVO_REFRESH_RATE_OPTION}})
+    system.registerMultimoduleProtocol("Hubsan", 2, {variants={"H107", "H301", "H501"}})
+    system.registerMultimoduleProtocol("OMP", 77, {features=TELEMETRY, options={RF_TUNE_OPTION}})
 end
 
 return {init=init}
