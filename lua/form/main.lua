d81eb153d2f6493259f4a4e5e31fb8734550ef1d
Bertrand Songis @ FrSky <50834778+bsongis-frsky@users.noreply.github.com>
2024-01-12 13:00:43 +0100

    Cosmetics

--- lua/form/main.lua
+++ lua/form/main.lua
@@ -7,9 +7,9 @@ end
 local function build(widget)
     form.create()
     local w, h = lcd.getWindowSize()
-    form.addTextButton(nil, {x=20, y=20, w=w/2, h=40}, "Press here", function() print("Button pressed") end)
-    form.addTextField(nil, {x=30, y=70, w=w/2, h=40}, function() return widget.text end, function(newValue) widget.text = newValue end)
-    form.addColorField(nil, {x=40, y=120, w=w/2, h=40}, function() return widget.color end, function(newValue) widget.color = newValue end)
+    form.addTextButton(nil, {x=20, y=20, w=math.max(170, w/2), h=40}, "Press here", function() print("Button pressed") end)
+    form.addTextField(nil, {x=30, y=70, w=math.max(170, w/2), h=40}, function() return widget.text end, function(newValue) widget.text = newValue end)
+    form.addColorField(nil, {x=40, y=120, w=math.max(170, w/2), h=40}, function() return widget.color end, function(newValue) widget.color = newValue end)
 end    
 
 local function paint(widget)
