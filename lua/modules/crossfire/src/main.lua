5350c1947f31e8094e58bb9d65ae7b9f00236993
Rob Thomson <rob@marotori.com>
2024-09-27 18:20:42 +0100

    Fix tab spacing in elrs and crsf scripts

--- lua/modules/crossfire/src/main.lua
+++ lua/modules/crossfire/src/main.lua
@@ -7,23 +7,23 @@ compile = assert(loadfile(config.moduleDir .. "compile.lua"))(config)
 elrs = assert(compile.loadScript(config.moduleDir .. "crossfire.lua"))(config, compile)
 
 local function create()
-    return crossfire.create()
+        return crossfire.create()
 end
 
 local function wakeup()
-    return crossfire.wakeup()
+        return crossfire.wakeup()
 end
 
 local function event(widget, category, value, x, y)
-    return crossfire.event(widget, category, value, x, y)
+        return crossfire.event(widget, category, value, x, y)
 end
 
 local function close()
-    return crossfire.close()
+        return crossfire.close()
 end
 
 local function init()
-    system.registerCrossfireModule({configure = {name = config.moduleName, create = create, wakeup = wakeup, event = event, close = close}})
+        system.registerCrossfireModule({configure = {name = config.moduleName, create = create, wakeup = wakeup, event = event, close = close}})
 end
 
 return {init = init}
