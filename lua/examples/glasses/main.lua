16564a00bc2511a06e37b69ba29d3379b898693a
bsongis <bsongis@gmail.com>
2026-02-03 16:52:34 +0100

    Lua glasses example

---
+++ lua/examples/glasses/main.lua
@@ -0,0 +1,23 @@
+-- Lua Glasses Test
+
+local function create()
+    return {layout=nil, armed=false}
+end
+
+local function build(context)
+    context.layout = glasses.createLayout({bitmap={id=10, x=10, y=10}, text={x=10, y=100}, border=true})
+    context.armed = false
+end
+
+local function wakeup(context)
+    if context.layout ~= nil and context.armed == false then
+        glasses.layoutClearAndDisplay(context.layout, "ARMED!")
+        context.armed = true
+    end
+end
+
+local function init()
+    system.registerGlassesWidget({key="engo", name="Engo Lua", create=create, build=build, wakeup=wakeup})
+end
+
+return {init=init}
