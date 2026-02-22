b72c616d1273c3e880690d53901aad9e0e1b91f2
Bertrand Songis <bsongis@gmail.com>
2026-02-22 15:25:14 +0100

    Lua ActiveLook script example updated

--- lua/examples/active-look/main.lua
+++ lua/examples/active-look/main.lua
@@ -5,13 +5,20 @@ local function create()
 end
 
 local function build(context)
-    context.layout = glasses.createLayout({bitmap={id=10, x=10, y=10}, text={x=10, y=100}, border=true})
+    w, h = glasses.getWindowSize()
+    context.layout = glasses.createLayout({x=10, y=10, w=w-20, h=h-20, bitmap={id=10, x=10, y=10}, text={x=10, y=60, font=1}, border=true})
     context.armed = false
 end
 
 local function wakeup(context)
     if context.layout ~= nil and context.armed == false then
-        glasses.layoutClearAndDisplay(context.layout, "ARMED!")
+        context.layout:clearAndDisplayExtended({x=10, y=10, text="text", commands={
+             {text={text="extra text 1", font=2, x=10, y=80}},
+             {text={text="extra text 2", font=3, x=10, y=110}},
+             {bitmap={id=10, x=80, y=10}}
+        }})
+        glasses.bitmap(150, 20, 10) -- out of layout bitmap
+        glasses.text(70, 70, 1, "out of layout text")
         context.armed = true
     end
 end
