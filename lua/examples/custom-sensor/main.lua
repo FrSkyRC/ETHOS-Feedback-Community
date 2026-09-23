e296ea8c13c9c2b8c85765ad13b8f98cd9fbd815
bsongis <bsongis@gmail.com>
2026-09-23 09:19:29 +0200

    Add "manufacturer" parameter to system.registerSensor()

--- lua/examples/custom-sensor/main.lua
+++ lua/examples/custom-sensor/main.lua
@@ -2,6 +2,7 @@
 
 local function init()
   system.registerSensor({
+    manufacturer = "My manufacturer",  -- optional: only usable in models where this manufacturer is selected (Model / Edit)
     appIdStart = 0x5100,               -- first S.Port appId this definition matches
     appIdEnd = 0x510F,                 -- last S.Port appId this definition matches (optional, defaults to appIdStart)
     name = "My sensor",                -- sensor name
@@ -13,7 +14,7 @@ local function init()
     userMax = 100.0,                   -- user-editable maximum value (optional, defaults to max)
     onDiscover = function(source)      -- called when the sensors Discover creates this sensor
       print(string.format("Hello world! I am sensor appId=0x%04X name=%s", source:appId(), source:name()))
-    end
+    end,    
   })
 end
 
